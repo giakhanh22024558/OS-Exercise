6d29f65c97e8e065f7ae7b3b31c778b595001a5b
giakhanh22024558 <127011768+giakhanh22024558@users.noreply.github.com>
2025-02-13 23:20:50 +0700

    Add files via upload

---
+++ simple.c
@@ -0,0 +1,39 @@
+/**
+ * simple.c
+ *
+ * A simple kernel module. 
+ * 
+ * To compile, run makefile by entering "make"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+       printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "GCD of 3300 and 24 is: %d\n", gcd(3300, 24));
+
+       return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+	printk(KERN_INFO "Removing Module\n");
+}
+
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
+
